7705993832f892af2c1e412fa62392bb4e23a7ec
Brett McCosham <brettmccosham@gmail.com>
2023-01-25 14:25:31 -0500

    initial commit

---
+++ FirstApplication/src/firstPackage/FirstPackage.java
@@ -0,0 +1,43 @@
+package firstPackage;
+import java.util.Scanner;
+public class FirstPackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Enter Your Name");
+			
+//			Scanner in = new Scanner(System.in);
+//			String s = in.nextLine();
+			
+// IF STATEMENTS-----------------------------------------------------------			
+//			if(s.equals("Brett")) {
+//				System.out.println("Hey Brett");
+//			} else if(s.equals("Emma")) {
+//				System.out.println("Hey Emma");
+//			} else {
+//				System.out.println("You're not welcome here");
+//			}
+
+//LOOPS ------------------------------------------------------------------
+//while loop 
+//			int i = 0; //initialization
+//			while(i < 10) { //condition
+//				System.out.println("i is: " +i);
+//				i++; //update
+//			}
+//			
+//for loop
+//			for(int i = 0; i < 10; i++) {
+//				System.out.println("i is: " +i);
+//			}
+//			in.close(); //Got to close the input
+//			}
+//do while loop -- does something atleast once
+			int i = 0;
+			do {
+				System.out.println("i is: " +i);
+				i++;
+			}while(i < 10);
+			
+	}
+}
